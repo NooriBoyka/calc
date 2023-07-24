2abc1d284273bcfeea6bba2381009e8431a88faf
mehdi boyka <mahdiN670@gmail.com>
2023-07-24 06:24:19 -0700

    first commit

---
+++ app/Http/Controllers/OperatorController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OperatorController extends Controller
+{
+    public function operation(Request $request)
+    {
+        // Receive Input
+        $input1 = $request->input('num1');
+        $input2 = $request->input('num2');
+        $inputOperator = $request->input('Operator');
+
+        // Operation
+        switch ($inputOperator) {
+            case "+":
+                echo $input1+$input2;
+                break;
+            case "-":
+                echo $input1-$input2;
+                break;
+            case "*":
+                echo $input1*$input2;
+                break;
+            case "/":
+                echo $input1/$input2;
+                break;
+            default;
+                echo 'shit';
+                break;
+        }
+    }
+}
